153db8681c763eeadbcd16e0b7fc6adfcf9317fd
devops@local <devops@local>
2026-03-13 16:40:18 +0700

    update

--- build/ubuntu-2404/code-server/setup.sh
+++ build/ubuntu-2404/code-server/setup.sh
@@ -27,11 +27,11 @@ sudo chmod 777 ~/.local/share/code-server/User/settings.json
 # support openshift
 sudo mkdir -p /.config/code-server/
 sudo chmod -R 777 /.config
-cp -f config/code-server-setting.yaml /.config/code-server/config.yaml
+sudo cp -f config/code-server-setting-insecure.yaml /.config/code-server/config.yaml
 sudo chmod -R 777 /.config
 sudo mkdir -p /.local/share/code-server/User/
 sudo chmod -R 777 /.local
-cp -f config/User/settings.json /.local/share/code-server/User/settings.json
+sudo cp -f config/User/settings.json /.local/share/code-server/User/settings.json
 sudo chmod -R 777 /.local
 
 echo "patch dpkg to not install when exist"
